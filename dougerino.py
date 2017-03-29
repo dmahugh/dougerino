40936050a77f56c8d655fe92f7da4db0161b19f9
Doug Mahugh <dmahugh@microsoft.com>
2017-03-28 18:56:11 -0700

    github_rest_api()

--- dougerino.py
+++ dougerino.py
@@ -13,6 +13,8 @@ import json
 import os
 import time
 
+import requests
+
 def bytecount(numbytes): #---------------------------------------------------<<<
     """Convert byte count to display string as bytes, KB, MB or GB.
 
@@ -286,6 +288,93 @@ def github_pagination(link_header): #----------------------------------------<<<
 
     return retval
 
+def github_rest_api(*, endpoint=None, auth=None, headers=None, #-------------<<<
+                    state=None, session=None):
+    """Call the GitHub API.
+
+    endpoint     = the HTTP endpoint to call; if endpoint starts with / (for
+                   example, '/orgs/microsoft'), it will be appended to
+                   https://api.github.com
+    auth         = optional authentication tuple - (username, pat)
+                   If not specified, the default gitHub account is
+                   setting('dougerino', 'defaults', 'github_user')
+    headers      = optional dictionary of HTTP headers to pass
+    state        = optional state object, where settings such as the session
+                   object are stored. If provided, must have properties as used
+                   below.
+    session      = optional Requests session object reference. If not provided,
+                   state.requests_session is the default session object. Use
+                   the session argument to override that default and use a
+                   different session. Use of a session object improves
+                   performance.
+
+    Returns the response object.
+
+    Sends the Accept header to use version V3 of the GitHub API. This can
+    be explicitly overridden by passing a different Accept header if desired.
+    """
+    if not endpoint:
+        print('ERROR: github_api() called with no endpoint')
+        return None
+
+    # set auth to default if needed
+    if not auth:
+        default_account = setting('dougerino', 'defaults', 'github_user')
+        if default_account:
+            auth = (default_account, setting('github', default_account, 'pat'))
+        else:
+            auth = () # no auth specified, and no default account found
+
+    # add the V3 Accept header to the dictionary
+    headers = {} if not headers else headers
+    headers_dict = {**{"Accept": "application/vnd.github.v3+json"}, **headers}
+
+    # make the API call
+    if session:
+        sess = session # explictly passed Requests session
+    elif state:
+        if state.requests_session:
+            sess = state.requests_session # Requests session on the state objet
+        else:
+            sess = requests.session() # create a new Requests session
+            state.requests_session = sess # save it in the state object
+    else:
+        # if no state or session specified, create a temporary Requests
+        # session to use below. Note it's not saved/re-used in this scenario
+        # so performance won't be optimized.
+        sess = requests.session()
+
+    sess.auth = auth
+    full_endpoint = 'https://api.github.com' + endpoint if endpoint[0] == '/' \
+        else endpoint
+    response = sess.get(full_endpoint, headers=headers_dict)
+
+    if state and state.verbose:
+        print('    Endpoint: ' + endpoint)
+
+    if state:
+        # update rate-limit settings
+        try:
+            state.last_ratelimit = int(response.headers['X-RateLimit-Limit'])
+            state.last_remaining = int(response.headers['X-RateLimit-Remaining'])
+        except KeyError:
+            # This is the strange and rare case (which we've encountered) where
+            # an API call that normally returns the rate-limit headers doesn't
+            # return them. Since these values are only used for monitoring, we
+            # use nonsensical values here that will show it happened, but won't
+            # crash a long-running process.
+            state.last_ratelimit = 999999
+            state.last_remaining = 999999
+
+        if state.verbose:
+            # display rate-limite status
+            username = auth[0] if auth else '(non-authenticated)'
+            used = state.last_ratelimit - state.last_remaining
+            print('  Rate Limit: {0} available, {1} used, {2} total for {3}'. \
+                format(state.last_remaining, used, state.last_ratelimit, username))
+
+    return response
+
 def hashkey(string, encoding='utf-8'): #-------------------------------------<<<
     """Return MD5 hex digest for a string value.
 
@@ -424,3 +513,17 @@ def yeardiff(fromdate=None, todate=None): #----------------------------------<<<
 #-------------------------------------------------------------------------------
 if __name__ == "__main__":
     pass # to do - unit tests
+
+
+    # test new github_rest_api()
+    USERNAME = 'dmahugh'
+    PAT = setting('github', USERNAME, 'pat')
+    print(USERNAME, PAT)
+    ENDPOINT = '/repos/dmahugh/gitdata/commits'
+    RESPONSE = github_rest_api(endpoint=ENDPOINT, auth=(USERNAME, PAT), headers={})
+    JSONDATA = json.loads(RESPONSE.text)
+    for commit in JSONDATA:
+        message = commit['commit']['message']
+        login = commit['author']['login']
+        thedate = commit['commit']['committer']['date']
+        print(thedate, login, message)
